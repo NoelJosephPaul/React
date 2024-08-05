c53d8b984da2b8cc703fbbb6c51f2e7f9209535a
Noel Joseph Paul <noeljpaul2002@gmail.com>
2024-08-05 18:59:51 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); 
+
+const parent = React.createElement("div",{id:"parent"},React.createElement("h1",{},"this is h1"));
+
+root.render(parent);
