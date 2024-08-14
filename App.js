c65112bc93c136ab48611b76d7d4601663d05991
Noel Joseph Paul <noeljpaul2002@gmail.com>
2024-08-14 21:52:37 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading); 
 
-const parent = React.createElement("div",{id:"parent"},React.createElement("h1",{},"this is h1"));
+//const parent = React.createElement("div",{id:"parent"},React.createElement("h1",{},"this is h1"));
 
-root.render(parent);
+//root.render(parent);
